d391f822974acc5280bb25d133cb59c7a1203231
Toxirjon <tokhiriy87@gmail.com>
2022-02-14 21:15:48 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+window.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector(".modal"),
+    modalContent = document.querySelector(".modal-content"),
+    btn = document.querySelector(".btn"),
+    closeBtn = document.querySelector(".close-btn");
+  const modalTimer = setTimeout(openModal, 3000);
+
+  // open Modal
+
+  btn.addEventListener("click", openModal);
+
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.scroll = "hidden";
+    clearInterval(modalTimer);
+  }
+
+  // close Modal
+
+  closeBtn.addEventListener("click", closeModal);
+
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.scroll = " ";
+  }
+
+  // close modal outside of the modal content
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+});
